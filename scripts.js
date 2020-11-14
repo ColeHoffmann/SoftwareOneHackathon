123ebfe90fa38964044d89e2fd8a4fbf1ec52f70
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2020-11-13 21:03:26 -0500

    updated getCountyStates

--- scripts.js
+++ scripts.js
@@ -18,35 +18,35 @@ var county;
 var OPI; 
 var Long;
 var Lat; 
-
+var stateJsonData
 //Fuction to set state and county
 function getCountyState(){
     var x = document.getElementById("frm1");
     county = x.elements[0].value;
     state = x.elements[1].value;
     console.log(county + state);
-}
-//This is the first and initiral API call, it will allow us to get all the data for our counties and departments from our initial HTML forms. 
-var StateDataURL = "https://api.usa.gov/crime/fbi/sapi/api/agencies/byStateAbbr/" + state + "//?API_KEY=" + crimeKey
-console.log(StateDataURL)
+   
+    //This is the first and initiral API call, it will allow us to get all the data for our counties and departments from our initial HTML forms. 
+    var StateDataURL = "https://api.usa.gov/crime/fbi/sapi/api/agencies/byStateAbbr/" + state + "//?API_KEY=" + crimeKey
+    console.log(StateDataURL)
 
-//Json data after the call 
-var stateJsonData;
 
-var request = new XMLHttpRequest(); 
-request.open(method, StateDataURL, true);
+    var request = new XMLHttpRequest(); 
+    request.open(method, StateDataURL, true);
 
-request.onload = function(){ 
-    console.log(request.status)
-    if (request.status >= 200 && request.status < 400){ 
-        stateJsonData = JSON.parse(this.response)
-        console.log(stateJsonData)
+    request.onload = function(){ 
+        console.log(request.status)
+        if (request.status >= 200 && request.status < 400){ 
+            stateJsonData = JSON.parse(this.response)
+            console.log(stateJsonData)
+        }
     }
-}
+    request.send()
+    console.log(stateJsonData)
+    }
 
-request.send()
 
-console.log(stateJsonData)
+
 
 //get /api/summarized/agencies/{ori}/offenses/{since}/{until} Agency level SRS Crime Data Endpoint
 
